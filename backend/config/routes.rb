d05ea5547284079463e5ea44acb6cae35099e0cc
daichi_sato <daichiexperience@gmail.com>
2022-03-19 12:40:17 +0900

    [add] post

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace 'api' do
     namespace 'v1' do
+      resources :microposts, only: %i[index create show update destroy]
       namespace 'auth' do
         post 'users' => 'users#create'
       end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_14_124956) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_014251) do
+  create_table "microposts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "uid", default: "", null: false
@@ -19,4 +27,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_14_124956) do
     t.index ["uid"], name: "index_users_on_uid", unique: true
   end
 
+  add_foreign_key "microposts", "users"
 end
